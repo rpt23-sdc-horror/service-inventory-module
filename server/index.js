f9ce12306f624396cc8590571652b86faae83cf6
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2020-10-07 23:54:50 -0700

    added cors module to server

--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ const inventoryDB = require('../database');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(function(req, res, next){
+     res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*'
+     })
+     next();
+});
 
 app.listen(3004, () => {
     console.log('Listening on port 3004');
 });
+
 
 app.get('/inventory/:productid/:styleid', (req, res) => {
     inventoryDB.findStyle(req.params.productid, req.params.styleid)
