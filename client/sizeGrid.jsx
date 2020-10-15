4715a3b91bdc527f55d67ab9f00706ae67d44307
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2020-10-15 01:36:25 -0700

    refactor to only use react to manipulate DOM

--- client/sizeGrid.jsx
+++ client/sizeGrid.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SizeTile from './components/sizeTile.jsx';
 import axios from 'axios';
-import $ from 'jquery';
 
 class SizeGrid extends React.Component {
   constructor(props) {
@@ -11,7 +10,8 @@ class SizeGrid extends React.Component {
       sizes: [],
       currentSize: '',
       currentTile: '',
-      noSizeSelected: false
+      didNotSelectSize: false,
+      fewSizesLeft: false
     };
   }
 
@@ -43,9 +43,7 @@ class SizeGrid extends React.Component {
   }
 
   selectSize (selectedSize) {
-
     const selected = document.getElementsByClassName(selectedSize)[0];
-
     if (this.state.currentSize) {
       this.state.currentTile.setAttribute('class', `sizeTile ${this.state.currentSize}`);
     }
@@ -54,11 +52,11 @@ class SizeGrid extends React.Component {
       selected.setAttribute('class', `selected ${selectedSize}`);
 
       if (this.hasFewLeft(this.getQuantity(this.state.currentTile))) {
-        if ($('#fewLeft').length == 0) {
-          $('#inventory').prepend('<div id="fewLeft">Just a few left. Order soon.</div>');
+        if (!document.getElementById('fewLeft')) {
+          this.setState({fewSizesLeft: true});
         }
       } else {
-        $('#fewLeft').remove();
+          this.setState({fewSizesLeft: false});
       }
     });
 
@@ -79,39 +77,44 @@ class SizeGrid extends React.Component {
 
   addToBag () {
     if (!this.state.currentSize) {
-      this.setState({noSizeSelected: true});
+      this.setState({didNotSelectSize: true});
     } else {
-      this.setState({noSizeSelected: false});
+      this.setState({didNotSelectSize: false});
     }
   }
 
   render() {
     let gridClass;
     let selectASize;
+    let orderSoon;
 
-    if (!this.state.currentSize && this.state.noSizeSelected) {
+    if (!this.state.currentSize && this.state.didNotSelectSize) {
       gridClass = 'grid grid-selected';
       selectASize = (<div className="inventory-text">Please select a size.</div>);
     } else {
       gridClass = 'grid';
     }
 
+    if (this.state.fewSizesLeft) {
+      orderSoon = (<div className="fewLeft">Just a few left. Order soon.</div>);
+    }
+
     return (
-      <div id="inventory">
-        <label id="selectSize" className="inventoryHeader">Select Size</label>
-        <label id="sizeGuide" className="inventoryHeader">Size Guide</label>
-        <div className={gridClass}>
-            {this.state.sizes.map(ele => <SizeTile sizes={ele} clickFunc={this.selectSize.bind(this)}/>)}
+        <div id="inventory">
+          {orderSoon}
+          <label id="selectSize" className="inventoryHeader">Select Size</label>
+          <label id="sizeGuide" className="inventoryHeader">Size Guide</label>
+          <div className={gridClass}>
+              {this.state.sizes.map(ele => <SizeTile sizes={ele} clickFunc={this.selectSize.bind(this)}/>)}
+          </div>
+          {selectASize}
+          <button className="inventory-button add-to-cart" onClick={this.addToBag.bind(this)}>Add to Bag</button>
+          <button className="inventory-button favorite">Favorite  ♡</button>
+          <div className="shipping-text">Shipping</div>
+          <div className="shipping-date">Arrives by Sun, Nov 1
+            <div className="edit-location">Edit Location</div>
+          </div>
         </div>
-        {selectASize}
-        <button className="inventory-button add-to-cart" onClick={this.addToBag.bind(this)}>Add to Bag</button>
-        <button className="inventory-button favorite">Favorite  ♡</button>
-        
-        <div className="shipping-text">Shipping</div>
-        <div className="shipping-date">Arrives by Sun, Nov 1
-          <div className="edit-location">Edit Location</div>
-        </div>
-      </div>
     );   
     
   }
