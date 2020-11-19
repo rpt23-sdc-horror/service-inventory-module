c90747ddfd1e5ff14d0c4cecdf2e92f8fb745a46
Martin <martinsung00@gmail.com>
2020-11-19 14:08:58 -0800

    Adjust endpoints, account for bad requests for all endpoints, update routes tests to test bad inputs.

--- test/src/server/routes.test.js
+++ test/src/server/routes.test.js
@@ -6,6 +6,17 @@ import { expect } from "chai";
 
 describe("Server Routes Tests", function () {
   const error = new Error("Fake Error");
+  const body = {
+    product_id: 2,
+    style_id: 1,
+    size: 12,
+    newQuantity: 9000,
+  };
+
+  const incorrectBody = {
+    product_id: null,
+    newQuantity: 9000,
+  };
 
   describe("Get Endpoint", function () {
     beforeEach(function () {
@@ -16,7 +27,7 @@ describe("Server Routes Tests", function () {
       sinon.restore();
     });
 
-    it("should return a 200 OK status", function (done) {
+    it("should return a 200 OK status for proper requests", function (done) {
       sinon.stub(Controller.prototype, "read").callsFake(async function () {
         const result = {
           rows: [
@@ -43,12 +54,39 @@ describe("Server Routes Tests", function () {
         });
     });
 
+    it("should return 404 status if either or both parameters are null", function (done) {
+      request(app)
+        .get(`/inventory/${null}/${null}`)
+        .expect(400)
+        .end(function (err) {
+          if (err) return done(err);
+        });
+
+      request(app)
+        .get(`/inventory/${undefined}/${undefined}`)
+        .expect(400)
+        .end(function (err) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
     it("should return 500 Internal Error status and log the error with Winston if the controller method fails", function (done) {
       sinon.stub(Controller.prototype, "read").throws(error);
 
       request(app)
         .get(`/inventory/${1}/${2}`)
         .expect(500)
+        .end(function (err) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
+    it("should not be able to call GET on the /inventory/product endpoint", function (done) {
+      request(app)
+        .get("/inventory/product")
+        .expect(405)
         .end(function (err) {
           if (err) return done(err);
           done();
@@ -64,6 +102,11 @@ describe("Server Routes Tests", function () {
       quantity: 300,
     };
 
+    const badDocument = {
+      product_id: null,
+      style_id: null,
+    };
+
     beforeEach(function () {
       sinon.restore();
     });
@@ -72,7 +115,7 @@ describe("Server Routes Tests", function () {
       sinon.restore();
     });
 
-    it("should return a 200 OK status", function (done) {
+    it("should return a 200 OK status for proper requests", function (done) {
       sinon.stub(Controller.prototype, "write").callsFake(async function () {
         const result = {
           rows: [
@@ -86,7 +129,7 @@ describe("Server Routes Tests", function () {
       });
 
       request(app)
-        .post("/inventory/product/add")
+        .post("/inventory/product")
         .send(document)
         .set("Accept", "application/json")
         .type("json")
@@ -99,11 +142,35 @@ describe("Server Routes Tests", function () {
         });
     });
 
+    it("should return a 400 Bad Request if the request body is incorrect", function (done) {
+      sinon.stub(Controller.prototype, "write").callsFake(async function () {
+        const result = {
+          rows: [],
+        };
+
+        return result;
+      });
+
+      request(app)
+        .post("/inventory/product")
+        .send(badDocument)
+        .set("Accept", "application/json")
+        .type("json")
+        .expect(400)
+        .end(function (err) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
     it("should return 500 Internal Error status and log the error with Winston if the controller method fails", function (done) {
       sinon.stub(Controller.prototype, "write").throws(error);
 
       request(app)
-        .post("/inventory/product/add")
+        .post("/inventory/product")
+        .send(document)
+        .set("Accept", "application/json")
+        .type("json")
         .expect(500)
         .end(function (err) {
           if (err) return done(err);
@@ -113,11 +180,6 @@ describe("Server Routes Tests", function () {
   });
 
   describe("Patch Endpoint", function () {
-    const body = {
-      size: 12,
-      newQuantity: 9000,
-    };
-
     beforeEach(function () {
       sinon.restore();
     });
@@ -126,7 +188,7 @@ describe("Server Routes Tests", function () {
       sinon.restore();
     });
 
-    it("should return a 200 OK status", function (done) {
+    it("should return a 200 OK status for proper requests", function (done) {
       sinon
         .stub(Controller.prototype, "updateQuantity")
         .callsFake(async function () {
@@ -144,7 +206,7 @@ describe("Server Routes Tests", function () {
         });
 
       request(app)
-        .patch(`/inventory/${1}/${1}/update`)
+        .patch("/inventory/product")
         .send(body)
         .set("Accept", "application/json")
         .type("json")
@@ -157,11 +219,34 @@ describe("Server Routes Tests", function () {
         });
     });
 
+    it("should return a 400 Bad Request if the request body is incorrect", function (done) {
+      sinon
+        .stub(Controller.prototype, "updateQuantity")
+        .callsFake(async function () {
+          const result = {
+            rows: [],
+          };
+
+          return result;
+        });
+
+      request(app)
+        .patch("/inventory/product")
+        .send(incorrectBody)
+        .set("Accept", "application/json")
+        .type("json")
+        .expect(400)
+        .end(function (err) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
     it("should return 500 Internal Error status and log the error with Winston if the controller method fails", function (done) {
       sinon.stub(Controller.prototype, "updateQuantity").throws(error);
 
       request(app)
-        .patch(`/inventory/${1}/${1}/update`)
+        .patch(`/inventory/product`)
         .send(body)
         .set("Accept", "application/json")
         .type("json")
@@ -182,7 +267,7 @@ describe("Server Routes Tests", function () {
       sinon.restore();
     });
 
-    it("should return a 200 OK status", function (done) {
+    it("should return a 200 OK status for proper requests", function (done) {
       sinon.stub(Controller.prototype, "delete").callsFake(async function () {
         const result = {
           rows: [
@@ -198,7 +283,10 @@ describe("Server Routes Tests", function () {
       });
 
       request(app)
-        .delete(`/inventory/${1}/${1}/${12}/delete`)
+        .delete("/inventory/product")
+        .send(body)
+        .set("Accept", "application/json")
+        .type("json")
         .expect(200)
         .expect("Content-type", /json/)
         .end(function (err, response) {
@@ -208,11 +296,35 @@ describe("Server Routes Tests", function () {
         });
     });
 
+    it("should return a 400 Bad Request if the request body is incorrect", function (done) {
+      sinon.stub(Controller.prototype, "delete").callsFake(async function () {
+        const result = {
+          rows: [],
+        };
+
+        return result;
+      });
+
+      request(app)
+        .delete("/inventory/product")
+        .send(incorrectBody)
+        .set("Accept", "application/json")
+        .type("json")
+        .expect(400)
+        .end(function (err) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
     it("should return 500 Internal Error status and log the error with Winston if the controller method fails", function (done) {
       sinon.stub(Controller.prototype, "delete").throws(error);
 
       request(app)
-        .delete(`/inventory/${1}/${1}/${12}/delete`)
+        .delete("/inventory/product")
+        .send(body)
+        .set("Accept", "application/json")
+        .type("json")
         .expect(500)
         .end(function (err) {
           if (err) return done(err);
