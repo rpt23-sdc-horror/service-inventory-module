f916adf47992dcaf32027b3fb2c1a82b47e3a9a9
Martin <martinsung00@gmail.com>
2021-01-05 15:01:06 -0800

    Pre-deployment set-up: edit port

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ import app from "./routes";
 import dotenv from 'dotenv'
 dotenv.config()
 
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
